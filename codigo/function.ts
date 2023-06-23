c2f5ae965ca4b34c7d6a1ed3b5631dff27e143a1
sajid2123 <sajidmalhi9@gmail.com>
2023-06-23 09:22:03 +0200

    hasta funciones

--- codigo/function.ts
+++ codigo/function.ts
@@ -1,5 +1,40 @@
-function addTwo(num: number){
+function  addTwo(num: number):number{
     return num + 2;
 }
-
 addTwo(5);
+
+function getUpper(val: string){
+    return val.toUpperCase;
+}
+
+function signUpUser(name: string,email:string,isPaid: boolean){
+
+}
+let loginUser = (name: string, email:string, isPaid: boolean) => {
+
+};
+
+function getValue(myVal: number){
+    if(myVal > 5){
+        return true;
+    }
+    return "200 OK"
+}
+
+const getHello = ():string => {
+    return "";
+}
+const heros = ["thoe","spiderman","batman"]
+
+heros.map((hero):string => {
+    return `hero is ${hero}`;
+})
+
+function consoleError(errmsg: string):void{
+    console.log(errmsg);
+}
+function handleError(errmsg: string): never{
+    throw new Error(errmsg);
+}
+
+
